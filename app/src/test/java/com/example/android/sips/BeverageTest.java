ea957c4d404bc76e14126a706cbba583f7023395
Matthew <matthewablonczy@gmail.com>
2018-12-10 15:57:42 -0500

    added unit testing comments

--- app/src/test/java/com/example/android/sips/BeverageTest.java
+++ app/src/test/java/com/example/android/sips/BeverageTest.java
@@ -11,17 +11,17 @@ public class BeverageTest {
     @Test
     public void createIngredientsString() {
         ArrayList<String> ingredients = new ArrayList<String>();
-        ingredients.add("1 oz vodka");
-        ingredients.add("1 oz lemon juice");
-        Beverage b = new Beverage(null, ingredients, null, null);
-        String result = b.createIngredientsString(ingredients);
-
+        ingredients.add("1 oz vodka");                                                     //in the API, there are two fields for measure (1 oz) and ingredient (vodka)
+        ingredients.add("1 oz lemon juice");                                               //a method in another class combines the two, but this one formats it to a string for display
+        Beverage b = new Beverage(null, ingredients, null, null); //this test is checking that the ingredient string is properly formatted
+        String result = b.createIngredientsString(ingredients);                            //a dash should be added to each element, as well as a newline character
+                                                                                           //for each element other than the last
         assertEquals("- 1 oz vodka\n- 1 oz lemon juice", result);
     }
 
     @Test
     public void createIngredientsString2() {
-        ArrayList<String> ingredients = new ArrayList<String>();
+        ArrayList<String> ingredients = new ArrayList<String>();  //this test is identical to the last, just checking for more elements
         ingredients.add("1 oz vodka");
         ingredients.add("1 oz tequila");
         ingredients.add("1 oz gin");
@@ -40,7 +40,7 @@ public class BeverageTest {
         ingredients.add("2 oz rum");
         ingredients.add("2 oz coke");
         ingredients.add("1/2 oz lime juice");
-        Beverage b = new Beverage(null, ingredients, null, null);
+        Beverage b = new Beverage(null, ingredients, null, null);  //again, identical to the last, just checking different number of elements
         String result = b.createIngredientsString(ingredients);
 
         assertEquals("- 2 oz rum\n- 2 oz coke\n- 1/2 oz lime juice", result);
@@ -49,7 +49,7 @@ public class BeverageTest {
     @Test
     public void createIngredientsString4() {
         ArrayList<String> ingredients = null;
-        Beverage b = new Beverage(null, ingredients, null, null);
+        Beverage b = new Beverage(null, ingredients, null, null);  //adding no ingredients should result in an empty string
         String result = b.createIngredientsString(ingredients);
 
         assertEquals("", result);
@@ -59,7 +59,7 @@ public class BeverageTest {
     public void createIngredientsString5() {
         ArrayList<String> ingredients = new ArrayList<>();
         ingredients.add("1.5 oz tequila");
-        Beverage b = new Beverage(null, ingredients, null, null);
+        Beverage b = new Beverage(null, ingredients, null, null);  //checking that a single element works properly
         String result = b.createIngredientsString(ingredients);
 
         assertEquals("- 1.5 oz tequila", result);
--- app/src/test/java/com/example/android/sips/CocktailAPITest.java
+++ app/src/test/java/com/example/android/sips/CocktailAPITest.java
@@ -8,28 +8,28 @@ public class CocktailAPITest {
 
     @Test
     public void buildURL() {
-        CocktailAPI c = new CocktailAPI(null);
-        String actual = c.buildURL("vodka");
+        CocktailAPI c = new CocktailAPI(null);                                                     //this test checks to make sure that the query URL is properly formatted
+        String actual = c.buildURL("vodka");                                                       //adding a single term should result in a proper URL
         assertEquals("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=vodka", actual);
     }
 
     @Test
     public void buildURL2() {
         CocktailAPI c = new CocktailAPI(null);
-        String actual = c.buildURL("moscow mule");
-        assertEquals("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=moscow_mule", actual);
+        String actual = c.buildURL("moscow mule");                                                      //search terms with a space need an _ to be searched properly
+        assertEquals("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=moscow_mule", actual); //otherwise, the query will be for just "moscow", which isn't technicall what we want
     }
 
     @Test
     public void buildURL3() {
         CocktailAPI c = new CocktailAPI(null);
-        String actual = c.buildURL("long island iced tea");
+        String actual = c.buildURL("long island iced tea");                                               //testing to see if it works for many terms
         assertEquals("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=long_island_iced_tea", actual);
     }
 
     @Test
     public void buildURL4() {
-        CocktailAPI c = new CocktailAPI(null);
+        CocktailAPI c = new CocktailAPI(null);                                                    //checking that trim() is working properly
         String actual = c.buildURL(" vodka");
         assertEquals("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=vodka", actual);
     }
@@ -37,7 +37,7 @@ public class CocktailAPITest {
     @Test
     public void buildURL5() {
         CocktailAPI c = new CocktailAPI(null);
-        String actual = c.buildURL("   vodka");
+        String actual = c.buildURL("   vodka   ");                                               //checking for even weirder case for trim()
         assertEquals("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=vodka", actual);
     }
 }
